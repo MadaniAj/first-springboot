e4f69fa7de270945da9a4bfc47b05c1148017204
madani <87623991+MadaniAj@users.noreply.github.com>
2023-03-04 18:00:31 +0100

    .

--- src/main/java/com/firstspring/firstspringboot/student/StudentController.java
+++ src/main/java/com/firstspring/firstspringboot/student/StudentController.java
@@ -2,7 +2,6 @@ package com.firstspring.firstspringboot.student;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +12,6 @@ public class StudentController {
 
     private final StudentService studentService;
 
-    @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
